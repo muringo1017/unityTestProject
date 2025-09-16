8b23a4ad44ddeb204dfe57d5e405d98aed9e5a37
muringo3 <muringo1017@gmail.com>
2025-09-16 15:44:04 +0900

    temp save

---
+++ Assets/00_Scripts/SingletonPattern/SingletonPattern.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SingletonPattern : MonoBehaviour
+{
+    private static SingletonPattern instance = null;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+             instance = this;
+             
+             DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
